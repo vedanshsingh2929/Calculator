be9a18380e186be611514f4c2931ab8de3e17fe0
vedanshsingh299 <singh.vedansh20@gmail.com>
2023-01-11 01:26:04 +0530

    first change targetted the buttons

--- calculator.js
+++ calculator.js
@@ -1,71 +1,26 @@
-let s = null;
-let a = null;
-let b = null;
-function one() {
-    console.log('1');
-    a = 1;
-    b = 1;
-}
-function two() {
-    console.log('2');
-    a = 2;
-    b = 2;
-}
-function three() {
-    console.log('3');
-    a = 3;
-    b = 3;
-}
-function four() {
-    console.log('4');
-    a = 4;
-    b = 4;
-}
-function five() {
-    console.log('5');
-    a = 5;
-    b = 5;
-}
-function six() {
-    console.log('6');
-    a = 6;
-    b = 6;
-}
-function seven() {
-    console.log('7');
-    a = 7;
-    b = 7;
-}
-function eigth() {
-    console.log('8');
-    a = 8;
-    b = 8;
-}
-function nine() {
-    console.log('9');
-    a = 9;
-    b = 9;
-}
-function zero() {
-    console.log('0');
-    a = 0;
-    b = 0;
-}
-function add() {
-    s = a + b;
-}
-function subtract() {
-    s = a - b;;
-}
-function multilpy() {
-    s = a * b;
-}
-function divide() {
-    s = a / b;
+let string = "";
+// function display(value)
+// {
+//     console.log("Value1",value);
+//     document.getElementsByClassName("input").value+=value;
+//     console.log(document.getElementsByClassName("button").value);
+// }
+let buttons = document.querySelectorAll('.button');
+// console.log("Buttons",buttons);
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+       console.log(e.target);
+       string = string + e.target.innerHTML;
+       document.querySelector('input').value = string;
+    })
+})
+
+function ac() {
+    string = "";
+    console.clear();
+    console.log(string);
+    document.querySelector('input').value = string;
 }
 function equal() {
-    console.log(s);
-}
-function ac() {
-    console.clear();
+    
 }
