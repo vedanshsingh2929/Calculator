b3f4378e669c29c44b7e3a896278ed8e5957fc1a
vedanshsingh299 <singh.vedansh20@gmail.com>
2023-01-10 18:35:49 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,71 @@
+let s = null;
+let a = null;
+let b = null;
+function one() {
+    console.log('1');
+    a = 1;
+    b = 1;
+}
+function two() {
+    console.log('2');
+    a = 2;
+    b = 2;
+}
+function three() {
+    console.log('3');
+    a = 3;
+    b = 3;
+}
+function four() {
+    console.log('4');
+    a = 4;
+    b = 4;
+}
+function five() {
+    console.log('5');
+    a = 5;
+    b = 5;
+}
+function six() {
+    console.log('6');
+    a = 6;
+    b = 6;
+}
+function seven() {
+    console.log('7');
+    a = 7;
+    b = 7;
+}
+function eigth() {
+    console.log('8');
+    a = 8;
+    b = 8;
+}
+function nine() {
+    console.log('9');
+    a = 9;
+    b = 9;
+}
+function zero() {
+    console.log('0');
+    a = 0;
+    b = 0;
+}
+function add() {
+    s = a + b;
+}
+function subtract() {
+    s = a - b;;
+}
+function multilpy() {
+    s = a * b;
+}
+function divide() {
+    s = a / b;
+}
+function equal() {
+    console.log(s);
+}
+function ac() {
+    console.clear();
+}
